c3d8882ed06d0cf4fdc981682f9d026ebc32061f
Aaron CHan <achn357@gmail.com>
2024-04-27 22:28:51 -0500

    Changing iter

--- random_runner.py
+++ random_runner.py
@@ -573,7 +573,7 @@ for trace in traces:
     url = f"https://dpc3.compas.cs.stonybrook.edu/champsim-traces/speccpu/{trace}"
     trace_fp = f"traces/{trace}"
     download_with_subprocess(url, trace_fp)
-    for i in range(5):
+    for i in range(3):
         print(i)
         try:
             main(i, trace)
